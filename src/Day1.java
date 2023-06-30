5587fedb759a8a71e98c752810cae2e3e9c0855b
cross <crosstown11223@yahoo.com>
2023-06-30 13:31:05 -0400

    Initial commit

---
+++ src/Day1.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+
+        // primitive and non-primitive data types
+        /**
+         * primitive:
+         * byte -> short -> int -> long -> float-> double -> char -> boolean
+         */
+        //non-primitive types
+        // String, Object, Wrapper Class Objects
+        // Wrapper Class is an Object type of primitive type versions
+      byte a;
+      short b;
+      int c;
+      long d;
+      float e;
+      double f;
+        System.out.println("Ranges");
+        System.out.println(Byte.MIN_VALUE + " -- "+ Byte.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE + " -- "+ Short.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE + " -- "+ Integer.MAX_VALUE);
+
+
+    }
+
+}
